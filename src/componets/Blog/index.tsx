136392137ccd3a0660b3fd6a0edeafc9c331adef
desaijenish <desaijenish4050@gmail.com>
2023-08-29 10:53:12 +0530

    changes

--- src/componets/Blog/index.tsx
+++ src/componets/Blog/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import BlogLogo from "../../asset/plug.png";
 import Bimage1 from "../../asset/blog1.jpg";
 import Bimage2 from "../../asset/blog2.jpg";
-import "./index.css";
 import BlogCards from "./BlogCards";
 import styled from "@emotion/styled";
 
@@ -17,6 +16,16 @@ const Width50 = styled.div(() => ({
     width: "100%",
   },
 }));
+const Container = styled.div( () => ({
+  display:'flex',
+  justifyContent:'space-between',
+  margin:'0 auto',
+  gap:'20px',
+  "@media (max-width: 900px)": {
+    flexDirection:'column',
+    alignItems:'center',
+  },
+}))
 const Blog = () => {
   return (
     <div>
@@ -25,14 +34,14 @@ const Blog = () => {
         <img src={BlogLogo} alt="" />
       </Heading>
       <div>
-        <div className="blog-container">
+        <Container className="blog-container">
           <Width50>
             <BlogCards image={Bimage1} />
           </Width50>
           <Width50>
             <BlogCards image={Bimage2} />
           </Width50>
-        </div>
+        </Container>
       </div>
     </div>
   );
