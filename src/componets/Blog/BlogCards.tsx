a82ccc5c0a8c34f71434a883819eccd5ccba720f
desaijenish <desaijenish4050@gmail.com>
2023-08-29 10:49:53 +0530

    css changes

--- src/componets/Blog/BlogCards.tsx
+++ src/componets/Blog/BlogCards.tsx
@@ -1,24 +1,36 @@
+import styled from "@emotion/styled";
 import React, { FC } from "react";
 interface BlogCardsProps{
     image:string
 }
 
+const BlogCard = styled.div( () => ({
+  width:'100%',
+  boxShadow:"0 0 10px 0 rgba(0,0,0,.29)",
+}) )
+const Heading = styled.h1( ()=>({
+  fontSize:'1.25rem',
+  margin:'10px 0px'
+}) )
+const Text = styled.p(()=>({
+  wordSpacing:'2px',
+  fontSize:'17px',
+  lineHeight:'24px'
+}) )
 const BlogCards:FC<BlogCardsProps> = ({image}) => {
   return (
     <div>
-
-        <div className="blogcard">
+        <BlogCard >
           <img src={image} alt="" width={"100%"} height={380} />
-          <div>
-            <h1>Blog Title Goes Here</h1>
-            <p className="blog-text">
+          <div style={{padding:'10px 30px 10px'}}>
+            <Heading>Blog Title Goes Here</Heading>
+            <Text >
               There are many variations of passages of Lorem Ipsum available,
               but the majority have suffered alteration in some form, by
               injected humour, or randomised
-            </p>
+            </Text>
           </div>
-        </div>
-
+        </BlogCard>
     </div>
   );
 };
