a6ea8775939b7a5dacb5bc4f1c99852ed0638b71
desaijenish <desaijenish4050@gmail.com>
2023-08-30 20:46:06 +0530

    change

--- src/componets/Footer/index.tsx
+++ src/componets/Footer/index.tsx
@@ -12,39 +12,88 @@ const RootContainer = styled.div( () =>({
    padding: "100px 0px",
 }) )
 
-// const Contact = styled.div( () => ({
-//   display:'flex',
-//   justifyContent:'center',
-// }) );
-
+const Container = styled.div( ()=>({
+  display:'flex',
+  gap:'50px',
+  justifyContent:'space-between',
+  width:"100%",
+  "@media (max-width:900px)":{
+    flexDirection:"column",
+    justifyContent:'center',
+    gap:'0px',
+    rowGap:'10px'
+  }
+}))
+const Email = styled.div( ()=>({
+  width:'80%',
+  "@media (max-width:900px)":{
+    width:'100%'
+  }
+}))
+const Input = styled.input(()=>({
+  margin:'20x 20px',
+  backgroundColor:'rgb(255, 255, 255)',
+  border:'none',
+  width:'60%',
+  height:'30px',
+  outline:'0px',
+  padding:'10px',
+  "@media (max-width:900px)":{
+    width:'calc(100% - 20px)',
+    margin:'0px',
+    marginTop:'20px'
+  }
+}) )
+const Center = styled.span(()=>({
+  display:'flex',
+  justifyContent:'center'
+}) )
+const Button = styled.button(() => ({
+  color: "white",
+  textDecoration: "none",
+  backgroundColor: "#4b208c",
+  border: "0px",
+  padding: "15px 50px",
+  borderRadius: "0px",
+  fontSize: "17px",
+  cursor: "pointer",
+  width:'25%',
+  marginLeft:'-20px',
+  "@media (max-width: 550px)": {
+    padding: "12px 40px",
+    borderRadius: "35px",
+    fontSize: "12px",
+    width:'70%',
+    margin:'10px 6px'
+  },
+}));
 const Footer = () => {
   return (
     <RootContainer>
-      <div className="footer-container">
+      <Container>
         <FooterContact/>
-      </div>
-      <div className="footer-container">
-        <div className="footer-email">
+      </Container>
+      <Container >
+        <Email>
           <span>
-            <input
+            <Input
               type="text"
-              className="inputFooter"
               placeholder="Enter your email"
             />
           </span>
-          <span className="center-footer">
-            <button
+          <Center >
+            <Button
               className="button-footer button"
               style={{ borderRadius: "0px" }}
             >
               SUBSCRIBE
-            </button>
-          </span>
-        </div>
+            </Button>
+          </Center>
+        </Email>
         <div className="w-20 flex" style={{ margin: "43px 0px" }}>
           <SocialMedia />
         </div>
-      </div>
+      </Container>
     </RootContainer>
   );
 };
