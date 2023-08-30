690d0b10aae1f7aa476a5f1a0a4b05cd96855a8c
desaijenish <desaijenish4050@gmail.com>
2023-08-30 21:52:11 +0530

    changes

--- src/componets/Footer/SocialMedia.tsx
+++ src/componets/Footer/SocialMedia.tsx
@@ -3,30 +3,44 @@ import { AiOutlineTwitter } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaFacebookF } from "react-icons/fa";
 import { BiLogoLinkedin } from "react-icons/bi";
+import styled from "@emotion/styled";
 
+
+const Box = styled.div(()=>({
+  width:'30px',
+  height:'30px',
+  backgroundColor:'white',
+  borderRadius:'50%',
+  margin:'0px 5px',
+  '@media (max-width:900px)':{
+    padding:10,
+    justifyContent:'center'
+  }
+}))
 function SocialMedia() {
+
   return (
     <div className="flex">
-      <div className="iconBox">
+      <Box className="iconBox">
         <center>
           <FaFacebookF className="icon-social" />
         </center>
-      </div>
-      <div className="iconBox">
+      </Box>
+      <Box className="iconBox">
         <center>
           <AiOutlineTwitter className="icon-social" />
         </center>
-      </div>
-      <div className="iconBox">
+      </Box>
+      <Box className="iconBox">
         <center>
           <BiLogoLinkedin className="icon-social" />
         </center>
-      </div>
-      <div className="iconBox">
+      </Box>
+      <Box className="iconBox">
         <center>
           <AiFillInstagram className="icon-social" />
         </center>
-      </div>
+      </Box>
     </div>
   );
 }
